26096c6cd96cd509229755cfc454ea21d8142ce1
Chris McKinney <ntd5@tachibanatech.com>
2018-11-22 09:34:03 -0500

    Now generates for trait Enum from pbirch::enums

--- src/lib.rs
+++ src/lib.rs
@@ -2,232 +2,9 @@
 
 #![recursion_limit = "128"]
 
-//! Generate enum repr conversions compatible with type aliases.
+//! **Do not use this crate directly, and do not push it to upstream.**
 //!
-//! Generate with `#[EnumRepr(type = "TYPE")]`.
-//!
-//! Functions generated are
-//! ```ignore
-//! fn repr(&self) -> EnumReprType
-//! fn from_repr(x: EnumReprType) -> Option<Self>
-//! ```
-//! The real enum discriminant is usually forced to be `#[repr(isize)]`.
-//! If `u*` or `i*` types are used for the discriminant, the actual enum
-//! representation is made to be `#[repr(that_type_specified)]`.
-//! The list of types recognized as `u*` and `i*` currently is as follows:
-//! `i8`, `i16`, `i32`, `i64`, `i128`, `u8`, `u16`, `u32`, `u64`, `u128`.
-//! If the type is specified through a type alias, `#[repr(isize)]` is used.
-//! Inability to specify type aliases as enum representations is this crate's
-//! reason to exist.
-//!
-//! The code generated does not require std.
-//!
-//! # Examples
-//! ```
-//! extern crate enum_repr;
-//! extern crate libc;
-//!
-//! use libc::*;
-//!
-//! use enum_repr::EnumRepr;
-//!
-//! #[EnumRepr(type = "c_int")]
-//! #[derive(Debug, PartialEq)]
-//! pub enum IpProto {
-//!     IP = IPPROTO_IP,
-//!     IPv6 = IPPROTO_IPV6,
-//!     // …
-//! }
-//!
-//! fn main() {
-//!     assert_eq!(IpProto::IP.repr(), IPPROTO_IP);
-//!     assert_eq!(IpProto::from_repr(IPPROTO_IPV6), Some(IpProto::IPv6));
-//!     assert!(IpProto::from_repr(12345).is_none());
-//! }
-//! ```
-//!
-//! ```
-//! # extern crate enum_repr;
-//! # extern crate libc;
-//! #
-//! # use libc::*;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//! #[EnumRepr(type = "c_int")]
-//! # #[derive(Debug, Eq, Hash, PartialEq)]
-//! pub enum InetDomain {
-//!     Inet = 2,
-//!     // …
-//! }
-//!
-//! #[EnumRepr(type = "c_int")]
-//! # #[derive(Debug, Eq, Hash, PartialEq)]
-//! pub enum SocketType {
-//!     Stream = 1,
-//!     // …
-//! }
-//!
-//! // …
-//!
-//! # fn main() { unsafe {
-//! assert!(
-//!    socket(InetDomain::Inet.repr(), SocketType::Stream.repr(), 0) != -1
-//! );
-//! # }}
-//! ```
-//!
-//! ```no_run
-//! # extern crate enum_repr;
-//! # extern crate libc;
-//! #
-//! # use libc::*;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//! // compatible with documentation and other attributes
-//!
-//! /// Represents a layer 3 network protocol.
-//! #[EnumRepr(type = "c_int")]
-//! #[derive(Debug, PartialEq)]
-//! pub enum IpProto {
-//!     IP = IPPROTO_IP,
-//!     IPv6 = IPPROTO_IPV6,
-//!     // …
-//! }
-//! #
-//! # fn main() {}
-//! ```
-//!
-//! Discriminants can be implicit if `implicit = true`:
-//! ```
-//! # extern crate enum_repr;
-//! # extern crate libc;
-//! #
-//! # use libc::*;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//!
-//! #[EnumRepr(type = "c_int", implicit = true)]
-//! #[derive(Debug, PartialEq)]
-//! pub enum Test {
-//!     A,
-//!     B,
-//!     C = 5,
-//!     D,
-//! }
-//!
-//! fn main() {
-//!     assert_eq!(Test::B.repr(), 1);
-//!     assert_eq!(Test::from_repr(6), Some(Test::D));
-//!     assert!(Test::from_repr(2).is_none());
-//! }
-//! ```
-//!
-//! Using implicit discriminants without setting the flag is an error:
-//! ```compile_fail
-//! # extern crate enum_repr;
-//! # extern crate libc;
-//! #
-//! # use libc::*;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//!
-//! #[EnumRepr(type = "c_int")]
-//! pub enum Test {
-//!     A,
-//!     B = 3
-//! }
-//! #
-//! # fn main() {}
-//! ```
-//!
-//! Take extra care to avoid collisions when using implicit discriminants:
-//! ```compile_fail
-//! # #![deny(overflowing_literals)]
-//! # extern crate enum_repr;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//! #[EnumRepr(type = "u8", implicit = true)]
-//! enum Test {
-//!     A = 1,
-//!     B,
-//!     C,
-//!     D = 3,
-//! }
-//! #
-//! # fn main() {}
-//! ```
-//!
-//! Out of bound discriminants fail to compile:
-//! ```compile_fail
-//! # #![deny(overflowing_literals)]
-//! # extern crate enum_repr;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//! #[EnumRepr(type = "u8")]
-//! enum Test {
-//!     A = 256
-//! }
-//! #
-//! # fn main() {}
-//! ```
-//!
-//! Even if they are implicit:
-//! ```compile_fail
-//! # #![deny(overflowing_literals)]
-//! # extern crate enum_repr;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//! #[EnumRepr(type = "u8", implicit = true)]
-//! enum Test {
-//!     A = 255,
-//!     B
-//! }
-//! #
-//! # fn main() {}
-//! ```
-//!
-//! Discriminants of a wrong type fail to compile as well:
-//! ```compile_fail
-//! # #![deny(overflowing_literals)]
-//! # extern crate enum_repr;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//! const C: u16 = 256;
-//!
-//! #[EnumRepr(type = "u8")]
-//! enum Test {
-//!     A = C
-//! }
-//! #
-//! # fn main() {}
-//! ```
-//!
-//! Using the actual enum discriminant representation:
-//! ```
-//! # extern crate enum_repr;
-//! #
-//! # use std::mem::size_of;
-//! #
-//! # use enum_repr::EnumRepr;
-//! #
-//! #[EnumRepr(type = "u8")]
-//! #[derive(Debug, PartialEq)]
-//! enum Test {
-//!     A = 1
-//! }
-//!
-//! fn main() {
-//!     assert_eq!(size_of::<u8>(), size_of::<Test>());
-//! }
-//! ```
+//! See [pbirch::enums](../pbirch/enums/index.html).
 
 extern crate proc_macro;
 extern crate proc_macro2;
@@ -281,7 +58,6 @@ pub fn EnumRepr(
 
 fn generate_code(input: &ItemEnum, repr_ty: &Ident) -> TokenStream {
     let ty = input.ident.clone();
-    let vis = input.vis.clone();
     let (names, discrs) = extract_variants(input);
     let vars_len = input.variants.len();
 
@@ -289,30 +65,34 @@ fn generate_code(input: &ItemEnum, repr_ty: &Ident) -> TokenStream {
         (names.clone(), discrs.clone(), discrs.clone());
     let repr_ty2 = repr_ty.clone();
     let repr_ty3 = repr_ty.clone();
+    let repr_ty4 = repr_ty.clone();
     let ty_repeat = iter::repeat(ty.clone()).take(vars_len);
     let ty_repeat2 = ty_repeat.clone();
     let repr_ty_repeat = iter::repeat(repr_ty.clone()).take(vars_len);
     let repr_ty_repeat2 = repr_ty_repeat.clone();
     let repr_ty_repeat3 = repr_ty_repeat.clone();
 
-    let (impl_generics, ty_generics, where_clause) =
-        input.generics.split_for_impl();
+    let generics_tuple = input.generics.split_for_impl();
+    let (impl_generics, ty_generics, where_clause) = generics_tuple.clone();
+    let (impl_generics2, ty_generics2, where_clause2) = generics_tuple;
 
     let ret: TokenStream = quote! {
-        impl #impl_generics #ty #ty_generics #where_clause {
-            #vis fn repr(&self) -> #repr_ty2 {
+        impl #impl_generics Enum<#repr_ty4> for #ty #ty_generics #where_clause {
+            fn repr(&self) -> #repr_ty2 {
                 match self {
                     #( #ty_repeat2::#names2 => #discrs2 as #repr_ty_repeat ),*
                 }
             }
 
-            #vis fn from_repr(x: #repr_ty3) -> Option<#ty> {
+            fn from_repr(x: #repr_ty3) -> Option<#ty> {
                 match x {
                     #( x if x == #discrs as #repr_ty_repeat2 => Some(#ty_repeat::#names), )*
                     _ => None
                 }
             }
+        }
 
+        impl #impl_generics2 #ty #ty_generics2 #where_clause2 {
             #[doc(hidden)]
             #[allow(dead_code)]
             fn _enum_repr_typecheck() {
